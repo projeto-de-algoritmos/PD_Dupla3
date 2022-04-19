603811ed6912d590a965ee34f5902e0dd8418f74
sofiapatrocinio <sofiapatrocinio@hotmail.com>
2022-04-18 22:04:53 -0300

    Show characters

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import knapSack from "./components/knapSack"
 import food from "./data/food"
-import characters from "./data/characters"
+import character from "./data/characters"
 import { useState } from 'react';
 import Card from 'react-bootstrap/Card'
 import ListGroup from 'react-bootstrap/ListGroup'
@@ -10,6 +10,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   const [foods, setFoods] = useState([]);
+  const [characters, setCharacters] = useState([]);
+  const [bestDistribution, setBestDistribution] = useState([]);
 
   return (
     <div className="App">
@@ -20,7 +22,7 @@ function App() {
       </header>
       <body>
         <div>
-          {foods.length === 0 ?
+          {foods.length === 0 && characters.length === 0 ?
             <div className="action mrg-top-500">
               <button className="graph-button mrg-right-10" type="button" onClick={() => { setFoods(knapSack.getRandomFoods(food.foods)) }}>
                 Sortear itens
@@ -28,24 +30,59 @@ function App() {
             </div>
             : null}
         </div>
-        {foods.length > 0 ? <div className="items">
-          {foods?.map((food) => {
-            return (
-              <div className="action">
-                <Card style={{ width: '10rem' }}>
-                  <Card.Img style={{ width: '40px' }} variant="top" src={food.link} />
-                  <Card.Body>
-                    <Card.Title>{food.name}</Card.Title>
-                  </Card.Body>
-                  <ListGroup className="list-group-flush">
-                    <ListGroupItem>Vida: {food.life}</ListGroupItem>
-                    <ListGroupItem>Peso: {food.weight}</ListGroupItem>
-                  </ListGroup>
-                </Card>
-              </div>
-            );
-          })}
-        </div> : null}
+        {foods.length > 0 && characters.length === 0 ?
+          <div>
+            <div className="items">
+              {foods?.map((food) => {
+                return (
+                  <div className="action">
+                    <Card style={{ width: '10rem' }}>
+                      <Card.Img style={{ width: '40px' }} variant="top" src={food.link} />
+                      <Card.Body>
+                        <Card.Title>{food.name}</Card.Title>
+                      </Card.Body>
+                      <ListGroup className="list-group-flush">
+                        <ListGroupItem>Vida: {food.life}</ListGroupItem>
+                        <ListGroupItem>Peso: {food.weight}</ListGroupItem>
+                      </ListGroup>
+                    </Card>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="action mrg-top-100">
+              <button className="graph-button mrg-right-10" type="button" onClick={() => { setFoods(knapSack.getRandomFoods(food.foods)) }}>
+                Sortear novamente
+              </button>
+              <button className="graph-button mrg-right-10" type="button" onClick={() => { setCharacters(character.characters) }}>
+                Escolher personagem
+              </button>
+            </div>
+          </div> : null}
+
+        {characters.length > 0 ?
+          <div>
+            <div className="items">
+              {characters?.map((character) => {
+                return (
+                  <div className="action">
+                    <Card style={{ width: '15rem' }}>
+                      <Card.Img style={{ width: '40px' }} variant="top" src={character.link} />
+                      <Card.Body>
+                        <Card.Title>{character.name}</Card.Title>
+                      </Card.Body>
+                      <ListGroup className="list-group-flush">
+                        <ListGroupItem>Mochila: {character.bag}</ListGroupItem>
+                      </ListGroup>
+                      <button className="card-button" type="button" onClick={() => { setBestDistribution(knapSack.knapSack(foods, character.bag)) }}>
+                        Selecionar
+                      </button>
+                    </Card>
+                  </div>
+                );
+              })}
+            </div>
+          </div> : null}
       </body>
     </div>
   );
